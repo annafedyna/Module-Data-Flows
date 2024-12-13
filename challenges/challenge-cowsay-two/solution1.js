5c04ca611487b7591c3ef4dcb81cf490a92a30d5
Anna Fedyna <annafedyna03@gmail.com>
2024-12-13 19:04:46 +0000

    cowsay implemented

--- challenges/challenge-cowsay-two/solution1.js
+++ challenges/challenge-cowsay-two/solution1.js
@@ -3,28 +3,50 @@
 // no libraries
 // https://nodejs.dev/learn/nodejs-accept-arguments-from-the-command-line
 // =================
-
+import cows from "cows";
 // 1. Accept arguments
 
 // how will you accept arguments?
+const args = process.argv.slice(2);
 
 // 2. Make supplies for our speech bubble
 
 let topLine = '_';
 let bottomLine = '-';
 let saying = '';
+saying = args.join(" ");
 
 // 3. Make a cow that takes a string
 
-function cowsay(saying) {
+
 // how will you make the speech bubble contain the text?
 
 // where will the cow picture go?
 
 // how will you account for the parameter being empty?
+function cowsay(saying) {
+  if (!saying) {
+    saying = "Moo!";
+  }
 
+  const bubbleWidth = saying.length + 2;
+
+  const bubbleTop =  `${topLine.repeat(bubbleWidth)} `;
+  const bubbleMiddle = `< ${saying} >`;
+  const bubbleBottom =  `${bottomLine.repeat(bubbleWidth)}` ;
+
+//   const cowPicture = `
+//          \\   ^__^
+            // \\  (oo)\\_______
+            //     (__)\\       )\\/\\
+            //         ||----w |
+            //         ||     ||
+//   `;
+  const randomCow = cows()[3];
+  return `${bubbleTop}\n${bubbleMiddle}\n${bubbleBottom}\n${randomCow}`;
 }
 
 //4. Pipe argument into cowsay function and return a cow
-
 // how will you log this to the console?
+
+console.log(cowsay(saying))
--- challenges/challenge-cowsay-two/solution2.js
+++ challenges/challenge-cowsay-two/solution2.js
@@ -5,14 +5,43 @@
 // =================
 
 // 1. Make  a command line interface.
+const readline = require("node:readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 // 2. Make supplies for our speech bubble
-
+const topLine = "_";
+const bottomLine = "-";
 // 3. Make a cow that takes a string
 
 const cow = (saying) => {
-    // how did you make the cow before?
-}
+    if (!saying) {
+        saying = "Moo!";
+    }
+
+    const bubbleWidth = saying.length + 2;
+
+    const bubbleTop =  `${topLine.repeat(bubbleWidth)} `;
+    const bubbleMiddle = `< ${saying} >`;
+    const bubbleBottom =  `${bottomLine.repeat(bubbleWidth)} `;
+
+    const cowPicture = `
+            \\   ^__^
+             \\  (oo)\\_______
+                 (__)\\       )\\/\\
+                    ||----w |
+                    ||     ||
+    `;
+
+    return `${bubbleTop}\n${bubbleMiddle}\n${bubbleBottom}\n${cowPicture}`;
+    }
+
 
 // 4. Use readline to get a string from the terminal 
 // (with a prompt so it's clearer what we want)
+rl.question(`What's cows say?`, (input) => {
+  console.log(cow(input));
+  rl.close();
+});
