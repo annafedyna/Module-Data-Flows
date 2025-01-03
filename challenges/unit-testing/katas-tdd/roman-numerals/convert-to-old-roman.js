073bf226cc1c970a085623c4ca10696959b5e0cb
Anna Fedyna <annafedyna03@gmail.com>
2025-01-03 19:22:54 +0000

    old roman encoder

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
@@ -1,3 +1,29 @@
-function convertToOldRoman(n) {}
+function convertToOldRoman(n) {
+  const roman = {
+    1: "I",
+    5: "V",
+    10: "X",
+    50: "L",
+    100: "C",
+    500: "D",
+    1000: "M",
+  };
+  res = "";
+  while (n > 0) {
+    for (const key of Object.keys(roman).reverse()) {
+        if (Math.floor(n / Number(key)) !== 0) {
+          const whole_part = Math.floor(n / Number(key))
+        for (let i = 0; i < whole_part; i++) {
+          res += roman[key];
+          n -= key;
+        }
+      } 
+    }
+  }
+    return res;
+}
+
+console.log(convertToOldRoman(3000));
+console.log(convertToOldRoman(19));
 
 module.exports = convertToOldRoman;
