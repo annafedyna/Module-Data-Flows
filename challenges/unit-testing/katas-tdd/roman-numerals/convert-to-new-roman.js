a6cfaaffc107aed6979749bd16cd6bba694c6168
Anna Fedyna <annafedyna03@gmail.com>
2025-01-03 18:26:41 +0000

    new roman encoder

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
@@ -1,3 +1,34 @@
-function convertToNewRoman(n) {}
+function convertToNewRoman(n) {
+     const roman = {
+       1: "I",
+       4: "IV",
+       5: "V",
+       9: "IX",
+       10: "X",
+       40: "XL",
+       50: "L",
+       90: "XC",
+       100: "C",
+       400: "CD",
+       500: "D",
+       900: "CM",
+       1000: "M",
+     };
+     res = "";
+     while (n > 0) {
+       for (const key of Object.keys(roman).reverse()) {
+         if (Math.floor(n / Number(key)) !== 0) {
+           while (n >= Number(key)) {
+             res += roman[key];
+             n -= Number(key);
+           }
+         }
+       }
+     }
+     return res;
+}
+
+console.log(convertToNewRoman(4));
+console.log(convertToNewRoman(19));
 
 module.exports = convertToNewRoman;
--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
@@ -2,6 +2,27 @@ let convertToNewRoman = require("./convert-to-new-roman");
 
 test("returns I if passed 1 as an argument", function () {
   // Arrange
+  const input = 1;
   // Act
+  const result = convertToNewRoman(input);
   // Assert
+  expect(result).toEqual("I");
+});
+
+test("returns II if passed 2 as an argument", function () {
+  // Arrange
+  const input = 2;
+  // Act
+  const result = convertToNewRoman(input);
+  // Assert
+  expect(result).toEqual("II");
+});
+
+test("returns LXV if passed 65 as an argument", function () {
+  // Arrange
+  const input = 64;
+  // Act
+  const result = convertToNewRoman(input);
+  // Assert
+  expect(result).toEqual("LXIV");
 });
